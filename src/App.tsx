19c055990921cbfcdbbdd5a98027b9a96758ba1f
luisredondo <luisround@gmail.com>
2020-10-29 23:51:14 -0600

    Sign of life

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleSubtract = () => {
+    setCounter(counter - 1);
+  };
+  
+  const handleAdd = () => {
+    setCounter(counter + 1);
+  };
+
   return (
-    <div>
-      Hello world.
-    </div>
+    <>
+      <h1>Hello world from Honduras.</h1>
+      <br/>
+      <p>This repository contains UNCERTAINTY.</p> 
+      <p>I know I want to do something here, but still do not know what.</p>
+      <br/>
+      <p>Meanwhile, a counter: </p>
+      <button onClick={handleSubtract}>-</button>
+      {` ${counter} `}
+      <button onClick={handleAdd}>+</button>
+    </>
   );
 }
 
